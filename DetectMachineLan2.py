48cbf6e86f1bbd9b37d1935a0cdf94e33a012e2c
apantojab <a.pantojab@gmail.com>
2017-04-30 00:21:53 +0200

    Changed version
    Updated fromm python 2 to 3

--- DetectMachineLan2.py
+++ DetectMachineLan2.py
@@ -9,6 +9,7 @@ import re
 import os
 import smtplib
 import time,datetime
+import gtk
 from optparse import OptionError
 from optparse import OptionGroup
 from optparse import OptionParser
@@ -54,13 +55,13 @@ class DetectMachineLan():
 
 	def __detectMachinesNetwork(self,ip):
 		machines = self.__scanNetwork(ip)
-		for k,v in machines['scan'].items(): 
+		for k,v in machines['scan'].iteritems(): 
 			if str(v['status']['state']) == 'up':
-				print("-------")
+				print "-------"
 				try:   
-					print(str(v['addresses']['ipv4'])+" --> "+str(v['addresses']['mac']))
+					print str(v['addresses']['ipv4'])+" --> "+str(v['addresses']['mac'])
 				except: 
-					print(str(v['addresses']['ipv4'])+" --> Mac no detected")
+					print str(v['addresses']['ipv4'])+" --> Mac no detected"
 
 	def __detectMachinesWhitelist(self,opts):
 		whitelist = self.__read_file()
@@ -69,7 +70,7 @@ class DetectMachineLan():
 
 		machines = self.__scanNetwork(opts.ip)
 
-		for k,v in machines['scan'].items(): 
+		for k,v in machines['scan'].iteritems(): 
 			if str(v['status']['state']) == 'up':
 				#print str(v)
 				try:   
@@ -95,7 +96,8 @@ class DetectMachineLan():
 						self.__writeLog(msg)
 		if opts.emailto:
 			self.__sendEmail(alert_mac,opts)
-		
+		if opts.gtk:
+			self.__gtkinfo(alert_mac)
 
 
 
@@ -130,6 +132,7 @@ class DetectMachineLan():
 		parser.add_option('--wl','--whitelist', action='store', default='whitelist.txt' , dest='whitelist_file', help='File have Mac whitelist ')
 		parser.add_option('-l','--log', action='store_true', default=False, dest='log', help='Log acctions script')
 		parser.add_option('-v','--verbose', action='store_true', default=False, dest='verbose', help='Verbose acctions script')
+		parser.add_option('-g','--gui', action='store_true', default=False, dest='gtk', help='GTK Windows with info')
 
 
 		parser.add_option_group(mac)
@@ -176,17 +179,26 @@ class DetectMachineLan():
 			if self.verbose:
 				self.__consoleMessage(debugemail)
 		problems = server.sendmail(opts.user, opts.emailto, message)
-		print(problems)
+		print problems
 		server.quit()
 
-	
-
+	def __gtkinfo(self,alert_mac):
+		parent = None
+		if alert_mac:
+			md = gtk.MessageDialog(parent, 
+				gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_WARNING, 
+				gtk.BUTTONS_CLOSE, 'List macs: \n '+str(alert_mac))
+		else:
+			md = gtk.MessageDialog(parent, 
+				gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, 
+				gtk.BUTTONS_CLOSE, "No intruders - All machines known")
+		md.run()
 
 
 	def __consoleMessage(self,message):
 		ts = time.time()
 		st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-		print('['+st+'] '+str(message))
+		print '['+st+'] '+str(message)
 
 
 	def __writeLog(self,log):
@@ -228,7 +240,7 @@ class DetectMachineLan():
 					if self.log:
 						self.__writeLog(msg) 
 				except IOError:
-					print() 
+					print 
 					msg = 'ERROR: Cannot open'+ self.whitelist_file
 					if self.verbose:
 						self.__consoleMessage(msg)
